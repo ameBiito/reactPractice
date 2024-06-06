6b78f2fee94d57ec18c2565ec000a34c94ceb207
Andrew <andreabf2001@gmail.com>
2024-06-06 16:49:38 +0200

    Added stuff

--- Projects/airbnb/src/App.js
+++ Projects/airbnb/src/App.js
@@ -3,12 +3,28 @@ import "./App.css"
 
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Card from "./components/Card";
+
+import rawCardData from "./data.js";
+
+const cardData = rawCardData.map((elem) => {
+    return (
+        <Card
+            key={elem.id}         //The key is required by react, it must be unique 
+            elem={elem}           //Now, in order to refer to the object properties, we need to do it as props.elem.something
+                                 // Same thing using spread syntax {...elem}, the difference is that we'll access them using prop.something
+        />
+    )
+})
 
 const App = () => {
     return(
         <div id="main-container">
             <Navbar />
             <Hero />
+            <section className="cards-list">
+                {cardData}
+            </section>
         </div>
     );
 };
--- Projects/airbnb/src/components/Card.js
+++ Projects/airbnb/src/components/Card.js
@@ -1,6 +1,25 @@
 import React from "react";
+import star from "../../public/assets/star.png"
 
 export default function Card(props){
+    let badgeText;
+    if(props.elem.openSpots === 0){
+        badgeText = "SOLD OUT"
+    } else if(props.elem.location === "Online"){
+        badgeText = "ONLINE"
+    }
     return(
+        <div className="card">
+            {badgeText && <div className="card-badge">{badgeText}</div>}
+            <img src={require(`../../public/assets/${props.elem.coverImg}`)} className="card--image" />
+            <div className="card--stats">
+                <img src={star} className="card--star" />
+                <span>{props.elem.stats.rating}</span>
+                <span className="gray">({props.elem.stats.reviewCount}) • </span>
+                <span className="gray">{props.elem.location}</span>
+            </div>
+            <p className="card--title">{props.elem.title}</p>
+            <p className="card--price"><span className="bold">From ${props.elem.price}</span> / person</p>
+        </div>
     );
 }
